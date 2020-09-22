7f7415833356b79071883cf8381835f8ddec6a7f
tamararuth <71681173+tamararuth@users.noreply.github.com>
2020-09-22 14:19:51 -0300

    Add files via upload
    Questões iniciais de Python

---
+++ Exercicios/RASbóticas - Tâmara/Questão 1.py
@@ -0,0 +1,7 @@
+nome = input("Qual o seu nome? ")
+idade = int(input("Qual é a sua idade? "))
+
+anoNascimento = 2020 - idade
+anoCem = anoNascimento + 100
+
+print(nome, "você fará 100 anos em:", anoCem)
---
+++ Exercicios/RASbóticas - Tâmara/Questão 3.py
@@ -0,0 +1,5 @@
+lista = [-5, -3, -2, 2, 3, 5, 7, 11, 13]
+
+for elemento in lista:
+    if elemento < 5:
+        print(elemento)
