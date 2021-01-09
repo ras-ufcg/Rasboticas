03c8e83e702c6e14aeca7a10f243f058bedb7934
Larissa Teixeira <62350941+lateixeiraa@users.noreply.github.com>
2021-01-09 19:05:33 -0300

    Delete ListaMenores10.py

--- Turma 1 - 2020/RASbotica/ListaMenores10.py
+++
@@ -1,9 +0,0 @@
-a = [1, 2, 5, 6, 8, 9, 10, 11, 12, 13]
-
-print("\n\nLista: \n")
-print(a)
-print("\nO programa ira exibir os numeros da lista acima menores que 10:\n")
-for elemento in a:
-    if elemento < 10:
-     print(elemento)
-
