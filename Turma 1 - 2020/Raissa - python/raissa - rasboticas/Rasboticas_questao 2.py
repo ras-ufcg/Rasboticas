473e1abd7ef11f1f3b6e1dab3338abb763ef9c53
Larissa Teixeira <62350941+lateixeiraa@users.noreply.github.com>
2021-01-09 19:06:49 -0300

    Delete Rasboticas_questao 2.py

--- Turma 1 - 2020/Raissa - python/raissa - rasboticas/Rasboticas_questao 2.py
+++
@@ -1,9 +0,0 @@
-'''Raissa Taina Pordeus Ferreira
-Questa 02 - informa se um numero e par ou impar'''
-numero=input("Informe um numero")
-numero=int(numero)
-numero=numero%2
-if numero == 0:
-  print("seu numero e par")
-elif numero!=0:
-  print("seu numero e impar")
