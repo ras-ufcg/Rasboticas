40b07b554cfc7d6ec066acf9c2e20784f9acc6ab
Larissa Teixeira <62350941+lateixeiraa@users.noreply.github.com>
2021-01-09 19:06:41 -0300

    Delete Rasboticas_questao 6.py

--- Turma 1 - 2020/Raissa - python/raissa - rasboticas/Rasboticas_questao 6.py
+++
@@ -1,8 +0,0 @@
-'''Raissa Taina Pordeus Ferreira
-Questa 06 -Esta questao pede uma string ao usuario e retorna se ela e palindroma ou nao'''
-p1=input("informe uma palavra: ")
-p2=p1[::-1]
-if p1 == p2:
-    print ("e um palindromo")
-else:
-    print ("nao e um palindromo")
