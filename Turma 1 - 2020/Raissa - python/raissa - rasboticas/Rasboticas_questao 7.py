3ab80189e61cd7160b88c3ca774a74e09c34b5c1
Larissa Teixeira <62350941+lateixeiraa@users.noreply.github.com>
2021-01-09 19:06:35 -0300

    Delete Rasboticas_questao 7.py

--- Turma 1 - 2020/Raissa - python/raissa - rasboticas/Rasboticas_questao 7.py
+++
@@ -1,7 +0,0 @@
-'''Raissa Taina Pordeus Ferreira
-Questa 07 -Esta questao pede para imprimir os numeros pares de uma lista'''
-lista = [2, 3, 4, 5, 7, 8, 12]
-for lista in lista:
-  a1 = lista % 2
-  if a1 == 0:
-    print(lista)
