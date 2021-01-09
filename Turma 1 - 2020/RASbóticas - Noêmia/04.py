026cc06672ff4bd884f140971ed70d9c9a10898a
Larissa Teixeira <62350941+lateixeiraa@users.noreply.github.com>
2021-01-09 19:09:19 -0300

    Delete 04.py

--- Turma 1 - 2020/RASbóticas - Noêmia/04.py
+++
@@ -1,7 +0,0 @@
-divisor = int(input("Digite um numero: "))
-lista_original = list(range(1, divisor+1))
-divisores = []
-for num in lista_original:
-    if divisor % num== 0:
-        divisores.append(num)
-print(divisores)
