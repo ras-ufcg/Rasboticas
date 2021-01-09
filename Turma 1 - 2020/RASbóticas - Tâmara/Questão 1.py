e6f53ee0e118e847c1f1f285482e1a5c517a850a
Larissa Teixeira <62350941+lateixeiraa@users.noreply.github.com>
2021-01-09 19:06:58 -0300

    Delete Questão 1.py

--- Turma 1 - 2020/RASbóticas - Tâmara/Questão 1.py
+++
@@ -1,7 +0,0 @@
-nome = input("Qual o seu nome? ")
-idade = int(input("Qual é a sua idade? "))
-
-anoNascimento = 2020 - idade
-anoCem = anoNascimento + 100
-
-print(nome, "você fará 100 anos em:", anoCem)
