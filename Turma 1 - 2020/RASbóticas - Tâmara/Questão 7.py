92fc7e89e30f62f90dacdad4c78bc3dc0fda35a2
Larissa Teixeira <62350941+lateixeiraa@users.noreply.github.com>
2021-01-09 19:08:30 -0300

    Delete Questão 7.py

--- Turma 1 - 2020/RASbóticas - Tâmara/Questão 7.py
+++
@@ -1,9 +0,0 @@
-numeros = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
-
-numerosPares = []
-
-for elemento in numeros:
-    if elemento % 2 == 0:
-        numerosPares.append(elemento)
-
-print(numerosPares)
