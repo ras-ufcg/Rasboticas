2de67ba138828b6c2b5d9c38697aa2af391c70af
noemiacintia <noemia.silva@ee.ufcg.edu.br>
2021-05-11 19:19:16 -0300

    Atualização de comentários do código

--- Turma 1 - 2021/Projeto - Rasboticas Movies/rasmovies.py
+++ Turma 1 - 2021/Projeto - Rasboticas Movies/rasmovies.py
@@ -1,15 +1,14 @@
-
 #---------------------------------------
 #Rasboticas Movies
 #Author: Projeto Rasboticas
 #10/05/2021 V.1
 #---------------------------------------
-# -*- coding: utf-8 -*-
+
 from time import sleep
 from sys import exit 
 import cv2
 
-imagem = cv2.imread("Semana 7.png")
+imagem = cv2.imread("logo.png")
 cv2.imshow("RASboticas", imagem)
 cv2.waitKey(0)
 #---------------------------------------
@@ -22,39 +21,38 @@ def exibeFilmes():
     print(i)
     sleep(0.5)
   arq_filmes.close()
-  main()
-  
-seus_filmes=[] #variável que armazena filmes assistidos
+  main()  
 
 #---------------------------------------
 #função que exibe/armazena os filmes que você assistiu com suas respectivas classificações
 #---------------------------------------
+seus_filmes=[] #variável que armazena filmes assistidos
 def filmesAssistidos():
-  
+  #MENU
   opc = int(input(''' Escolha uma opção:
   [1] - Adicionar um novo Filme/Classificação
   [2] - Exibir Filmes/Classificação
   [3] - Menu Principal
   '''))
-  if opc == 1:
+  if opc == 1: #Adicionar novo Filme
     arquivo = open("seus_filmes.txt", "a") #criando o arquivo que armazena seus filmes/notas
 
     name = input('Digite o nome do filme assistido: ')
     classificacao = input('Digite sua classificacao 0-5:')
-    seus_filmes.append(name)
-    seus_filmes.append(' ')
-    seus_filmes.append(classificacao)
+    seus_filmes.append(name) #Adc o nome do filme a lista 'SEUS FILMES'
+    seus_filmes.append(' ') 
+    seus_filmes.append(classificacao)#Adc a classificacao do filme a lista 'SEUS FILMES'
     seus_filmes.append('\n') 
-    arquivo.writelines(seus_filmes)
+    arquivo.writelines(seus_filmes) #inserindo os dados da lista'Seus Filmes' no arquivo.
     arquivo.close()
 
-  elif opc == 2:
-    arquivo = open("seus_filmes.txt", "r")
+  elif opc == 2: #Exibir filmes/classificação
+    arquivo = open("seus_filmes.txt", "r") #Abrindo o arquivo com filmes/classificacao para leitura
     #Exibindo seus filmes/notas
     while True:
-      j=0
+      j=0 #Ordenar os filmes
       for i in arquivo:
-        val=i.split()
+        val=i.split() #Separar nomes/classificação
         print('[', j+1,']', val[0],'\nNota:', val[1],'\n')
         j+=1
       arquivo.close()
@@ -63,9 +61,9 @@ def filmesAssistidos():
       main()
     
     
-  elif opc == 3:
+  elif opc == 3: #Menu Principal
     main()
-  else:
+  else: #Se digitar um numero errado
     print('ERROR 404 NOT FOUND')
     print('Retornando ao Menu Principal...')
     sleep(1)
@@ -84,12 +82,11 @@ def conhecaProjeto():
 #---------------------------------------
 def instrucoes():
   #arq_intrucoes=("instrucoes.txt", "a")
-  arq_intrucoes = open("instrucoes.txt", "r")
-  for i in arq_intrucoes.readlines():
+  arq_intrucoes = open("instrucoes.txt", "r") #Abrindo o arquivo com instruções para leitura
+  for i in arq_intrucoes.readlines(): #Ler linha por linha
     print(i)
     sleep(0.5)
   arq_intrucoes.close()
-
 
   main()
 #---------------------------------------
