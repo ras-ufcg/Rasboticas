7c899a12ac96e21ecf993330d0d02e1c4ba0e325
Larissa Teixeira <larissa.silva@ee.ufcg.edu.br>
2023-01-25 15:23:58 -0300

    Delete Turma 2 - 2021 directory

--- Turma 2 - 2021/Rasboticas - Emilly/Ex.3.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-a = 1
-
-while (a != 0):
-  num = float(input("Digite um número que deseja obter o seno e o cosseno: "))
-  seno = np.sin(num)
-  cos = np.cos(num)
-  print("Seno: {}, Cosseno: {}, Numero: {}".format(seno, cos, num))
-  a = int(input("Deseja continuar? 1 - sim / 0 - não"))
